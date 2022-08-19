1cc8e49acba9e58702ce110ca90324ff8bd1be85
James van de Ven <hello@jamesvandeven.com>
2022-08-19 10:14:26 +0800

    Added supabase client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { Container } from "@mui/system";
 import type { AppProps } from "next/app";
+import supabase from "utils/supabase";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = supabase;
+
+  console.log(client);
+
   return (
     <Container maxWidth="lg">
       <Component {...pageProps} />
---
+++ utils/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Cannot create Supabase client");
+  throw Error;
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;

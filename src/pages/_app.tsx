ddb8b413582a86f44dc0ec42e8cb25f06a765143
James van de Ven <hello@jamesvandeven.com>
2022-08-21 23:03:08 +0800

    Added signout and fixed hyrdation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,26 @@
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 import useUserStore from "src/hooks/auth";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const refreshSession = useUserStore((state) => state.refreshSession);
-
-  useEffect(() => {
-    refreshSession();
-  }, []);
+  const user = useUserStore((state) => state.user);
+  const signOut = useUserStore((state) => state.signOut);
 
   return (
     <Container maxWidth="lg">
+      {user && (
+        <Button
+          variant="outlined"
+          onClick={signOut}
+          sx={{
+            position: "fixed",
+            top: 16,
+            right: 16,
+          }}
+        >
+          Sign Out
+        </Button>
+      )}
       <Component {...pageProps} />
     </Container>
   );

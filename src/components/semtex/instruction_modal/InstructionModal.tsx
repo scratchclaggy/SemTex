e47b63fa209b7c4cc7d176e2797f0d1386df96d3
Aswintvinod <22870036@student.uwa.edu.au>
2022-08-24 00:12:37 +0800

    Basic Modal component Working

--- src/components/semtex/instruction_modal/InstructionModal.tsx
+++ src/components/semtex/instruction_modal/InstructionModal.tsx
@@ -29,66 +29,7 @@ const text= `Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
 dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
 consectetur ac, vestibulum at eros.
 
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-ras mattis consectetur purus sit amet fermentum. Cras justo odio,
-dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-consectetur ac, vestibulum at eros.
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-ras mattis consectetur purus sit amet fermentum. Cras justo odio,
-dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-consectetur ac, vestibulum at eros.
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-ras mattis consectetur purus sit amet fermentum. Cras justo odio,
-dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-consectetur ac, vestibulum at eros.
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-ras mattis consectetur purus sit amet fermentum. Cras justo odio,
-dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-consectetur ac, vestibulum at eros.
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.
-ras mattis consectetur purus sit amet fermentum. Cras justo odio,
-dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-consectetur ac, vestibulum at eros.
-
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-
-Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-ullamcorper nulla non metus auctor fringilla.end`
+`
 const BootstrapDialogTitle = (props: DialogTitleProps) => {
   const { children, onClose, ...other } = props;
 

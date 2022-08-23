3f65236b85f5f49854b08be06deafa5774834dcc
Tyler Condit <tyreid27@users.noreply.github.com>
2022-08-23 11:36:43 +0800

    updates to types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,61 +1,53 @@
-export
-type User = {
-    id: string;
-    password: string;
-    email: string;
-    type: string;
-}
+export type User = {
+  id: string;
+  password: string;
+  email: string;
+  type: string;
+};
 
-export
-type DatasetPermission = {
-    id: string;
-    userID: string;
-    datasetID: string;
-}
+export type DatasetPermission = {
+  id: string;
+  userID: string;
+  datasetID: string;
+};
 
-export
-type DataSet = {
-    id: string;
-    instructions: string;
-    name: string;
-    created: string;
-}
+export type DataSet = {
+  id: string;
+  instructions: string;
+  name: string;
+  created: string;
+};
 
-export
-type ResponseOption = {
-    id: string;
-    label: string;
-    datasetID: string;
-}
+export type ResponseOption = {
+  id: string;
+  label: string;
+  datasetID: string;
+};
 
-export
-type HighlightOption = {
-    id: string;
-    label: string;
-    color: string;
-    datasetID: string;
-}
+export type HighlightOption = {
+  id: string;
+  label: string;
+  color: string;
+  datasetID: string;
+};
 
-export
-type TextSample = {
-    id: string;
-    body: string;
-    datasetID: string;
-}
+export type TextSample = {
+  id: string;
+  body: string;
+  datasetID: string;
+};
 
-export
-type Highlight = {
-    id: string;
-    sample: string;
-    highlightOptionID: string;
-    userResponseID: string;
-}
+export type Highlight = {
+  id: string;
+  sample: string;
+  highlightOptionID: string;
+  userResponseID: string;
+};
 
-export
-type UserResponse = {
-    id: string;
-    response: string;
-    comments: string;
-    userID: string;
-    textSampleID: string;
-}
+export type UserResponse = {
+  id: string;
+  response: string;
+  comments: string;
+  userID: string;
+  textSampleID: string;
+};

c7489d836e6c55662db52e0dbf7c03563a81226e
Michael <m.m.downing@bigpond.com>
2022-09-02 23:30:57 +0800

    Basic textarea box and submit function constructed

--- src/components/semtex/CommentInput.tsx
+++ src/components/semtex/CommentInput.tsx
@@ -1,5 +1,28 @@
+import { Box } from "@mui/material";
+
 const CommentInput = () => {
-  return <div>CommentInput</div>;
+
+  function handleChange ()
+  {
+    console.log("Key Stroke")
+  }
+
+  return (
+    <Box>
+      <form>
+      <textarea
+      onChange={handleChange}
+      style={{
+        width:"800px",
+        height:"200px",
+        fontSize:"15px",
+        resize:"none"
+      }}
+      >
+      </textarea>
+    </form>
+    </Box>
+  )
 };
 
 export default CommentInput;

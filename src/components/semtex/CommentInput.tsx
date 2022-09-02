2309dfae825a057fab9289ddece7b498cb3f1253
Michael <m.m.downing@bigpond.com>
2022-09-02 23:43:04 +0800

    I dont know what happened but it wants me to save chagnes

--- src/components/semtex/CommentInput.tsx
+++ src/components/semtex/CommentInput.tsx
@@ -10,17 +10,17 @@ const CommentInput = () => {
   return (
     <Box>
       <form>
-      <textarea
-      onChange={handleChange}
-      style={{
-        width:"800px",
-        height:"200px",
-        fontSize:"15px",
-        resize:"none"
-      }}
-      >
-      </textarea>
-    </form>
+        <textarea
+        onChange={handleChange}
+        style={{
+          width:"800px",
+          height:"200px",
+          fontSize:"15px",
+          resize:"none"
+        }}
+        >
+        </textarea>
+      </form>
     </Box>
   )
 };

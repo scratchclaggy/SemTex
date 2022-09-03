813c5e6fddfe14c53c63cddf9203712da45c42b6
Aswintvinod <22870036@student.uwa.edu.au>
2022-09-03 11:10:58 +0800

    Completely working progress bar

--- src/components/semtex/Progress.tsx
+++ src/components/semtex/Progress.tsx
@@ -1,38 +1,24 @@
 import * as React from 'react';
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
+import LinearProgress, { LinearProgressProps } from  '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box sx={{ width: '100%', mr: 1 }}>
-        <LinearProgress variant="determinate" {...props} />
-      </Box>
-      <Box sx={{ minWidth: 35 }}>
+const ProgressBar = () => {
+  // TODO: Get user responses from DB
+const    completed= 28;
+    const count= 57
+
+  const percentage = completed / count * 100;
+  return <Box sx={{ display: 'flex', alignItems: 'center' }}>
+  <Box sx={{ width: '100%', mr: 1 }}>
+    <LinearProgress variant="determinate" value={percentage} />
+  </Box>
+  <Box sx={{ minWidth: 35 }}>
         <Typography variant="body2" color="text.secondary">{`${Math.round(
-          props.value,
+          percentage,
         )}%`}</Typography>
       </Box>
-    </Box>
-  );
+</Box>
 }
 
-export default function LinearWithValueLabel() {
-  const [progress, setProgress] = React.useState(10);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 10));
-    }, 800);
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  return (
-    <Box sx={{ width: '100%' }}>
-      <LinearProgressWithLabel value={progress} />
-    </Box>
-  );
-}
+export default ProgressBar

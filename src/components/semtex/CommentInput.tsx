0b946823afa109af648d6941ddf637a917b9b8a3
Michael <m.m.downing@bigpond.com>
2022-09-04 17:36:58 +0800

    tidied up for PR Review

--- src/components/semtex/CommentInput.tsx
+++ src/components/semtex/CommentInput.tsx
@@ -1,33 +1,30 @@
-import { Box, StepContext } from "@mui/material";
-import React, {useState, useEffect} from "react";
-import {useForm} from "react-hook-form";
-import debounce from 'lodash/debounce';
+import { Box } from "@mui/material";
+import debounce from "lodash/debounce";
+import React, { useState } from "react";
 
 const CommentInput = () => {
-
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
   // setText(event.target.value) <- console.log below will be replaced with this function
   const handleInput = debounce((event) => {
     console.log(event.target.value);
   }, 300);
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    handleInput(event)
+    handleInput(event);
   };
 
   return (
     <Box>
       <form>
         <textarea
-        onChange={handleChange}
-        style={{
-          width:"800px",
-          height:"200px",
-          fontSize:"15px",
-          resize:"none"
-        }}
-        >
-        </textarea>
+          onChange={handleChange}
+          style={{
+            width: "800px",
+            height: "200px",
+            fontSize: "15px",
+            resize: "none",
+          }}
+        ></textarea>
       </form>
     </Box>
   );

41d761b1dc3088f160a6ac5710be89d2b849196a
Michael <m.m.downing@bigpond.com>
2022-09-06 17:11:19 +0800

    Everything except property passed between parent and child complete

--- src/components/semtex/history/History_Card.tsx
+++ src/components/semtex/history/History_Card.tsx
@@ -2,65 +2,63 @@ import { Box, Typography, Grid, IconButton } from "@mui/material";
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import AssignmentLateIcon from '@mui/icons-material/AssignmentLate';
 import { useState } from 'react';
-  
-function handleClick()
-  {
-    console.log("Button pressed!")
+
+
+
+const History_Card = (body: String) => {
+
+  function handleClick() {
+    console.log(console.log("Button Pressed"))
   }
+  const [clicked, setClicked] = useState(false);
 
-
-const History_Card = (props: String) => {
-
-    const [clicked, setClicked] = useState(false);
-
-    return (
+  return (
     <Box>
-    {
-      (
-        <Grid
-        sx={{
-          display:'flex',
-        }}
-        >
-          <Typography
-          onClick={handleClick}
-          noWrap
-          sx={{
-            padding: "5px",
-            borderBottom: 1,
-            '&:hover':{
-            borderRight: 1
-            }
-          }}
+      {
+        (
+          <Grid
+            sx={{
+              display: 'flex',
+            }}
           >
-          {props}
-          </Typography>
-
-          <IconButton onClick={() => setClicked(prevClick => !prevClick)}>
-            {clicked?
-              <AssignmentTurnedInIcon
-              style={{
-                paddingTop: '5px',
-                color: 'green',
-                alignItems: 'center'
+            <Typography
+              onClick={handleClick}
+              noWrap
+              sx={{
+                padding: "5px",
+                borderBottom: 1,
+                '&:hover': {
+                  borderRight: 1
+                }
               }}
-              ></AssignmentTurnedInIcon>
-              :
-              <AssignmentLateIcon
-              style={{
-                paddingTop: '5px',
-                color: 'red',
-                alignItems: 'center'
-              }}
-              ></AssignmentLateIcon>
-            }
-          </IconButton>
+            >
+            </Typography>
 
-        </Grid>
-      )
-    }
+            <IconButton onClick={() => setClicked(prevClick => !prevClick)}>
+              {clicked ?
+                <AssignmentTurnedInIcon
+                  style={{
+                    paddingTop: '5px',
+                    color: 'green',
+                    alignItems: 'center'
+                  }}
+                ></AssignmentTurnedInIcon>
+                :
+                <AssignmentLateIcon
+                  style={{
+                    paddingTop: '5px',
+                    color: 'red',
+                    alignItems: 'center'
+                  }}
+                ></AssignmentLateIcon>
+              }
+            </IconButton>
+
+          </Grid>
+        )
+      }
     </Box>
-);
+  );
 };
 
 export default History_Card;

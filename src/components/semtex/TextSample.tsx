e47c51f6c8446dc54068528557e6994c7db87d3b
Michael <m.m.downing@bigpond.com>
2022-09-07 16:28:05 +0800

    Text highlighting WIP

--- src/components/semtex/TextSample.tsx
+++ src/components/semtex/TextSample.tsx
@@ -1,19 +1,50 @@
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TextSample = () => {
-  const [text, setText] = useState("Lorem ipsum dolor sit amet consectetur")
-  const test = [<mark>Testing</mark>, <Typography>Second Test</Typography>]
+  const text = "Lorem ipsum dolor sit amet consectetur"
+  const [markup, setMarkup] = useState(text)
+  const [selection, setSelection] = useState('')
+  const [beforeSelection, setBeforeSelection] = useState('')
+  const [afterSelection, setAfterSelection] = useState('')
+
+
+  const output: any = [] 
+
+  useEffect(() => {
+      
+  }, [afterSelection])
+
+
+
 
   const highlighterHandler = () =>{
     const selObj = window.getSelection()
-    const selText = selObj.toString()
-    
+    if(selObj != null){
+      let start: number = selObj.anchorOffset
+      let end: number = selObj.focusOffset
 
-    // This will be replaced with a function to export the saved text
-    // Currently this hands out ANY highlighted text, provided you release the mouse
-    // within the range of the Box. 
-    console.log(selText);
+      // Allow the user to highlight text backwards
+      if(start > end){
+        let temp = end
+        end = start
+        start = temp
+      }
+
+      const before= text.substring(0, start)
+      const after = text.substring(end, text.length)
+      const select = selObj.toString()
+
+      setBeforeSelection(before)
+      setAfterSelection(after)
+      setSelection(select)
+      
+    }
+    output.push(beforeSelection)
+    output.push(<mark>{selection}</mark>)
+    output.push(afterSelection)
+
+    setMarkup(output)
   }
 
   return (
@@ -25,7 +56,7 @@ const TextSample = () => {
       padding: '4px'
     }}
     >
-      <Typography onMouseUp={highlighterHandler}>{test}</Typography>
+      <Typography onMouseUp={highlighterHandler}>{markup}</Typography>
     </Box>
   );
 };

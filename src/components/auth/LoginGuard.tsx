32b969e8648024cfcc646b18ac8bc8b6a19e6375
James van de Ven <hello@jamesvandeven.com>
2022-09-09 14:14:02 +0800

    Add login guard

---
+++ src/components/auth/LoginGuard.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import useAuth from "src/contexts/AuthContext";
+
+const LoginGuard = ({ children }: { children: JSX.Element }) => {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const unprotectedPaths = ["/sign-in", "/sign-up"];
+
+  if (user || unprotectedPaths.some((path) => path === router.pathname)) {
+    return <>{children}</>;
+  }
+
+  router.push('/sign-in')
+
+  return null;
+};
+
+export default LoginGuard;

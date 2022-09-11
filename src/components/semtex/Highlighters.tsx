76ed2d945ee0cf6277c875a91c4a8845981bae1b
Michael <m.m.downing@bigpond.com>
2022-09-11 21:50:28 +0800

    basic formatting changes

--- src/components/semtex/Highlighters.tsx
+++ src/components/semtex/Highlighters.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 const Highlighters = () => {
 
@@ -12,7 +12,7 @@ const Highlighters = () => {
       margin:'4px'
     }}
     >
-
+      <Stack></Stack>
     </Box>
   );
 };
--- src/components/semtex/TextSample.tsx
+++ src/components/semtex/TextSample.tsx
@@ -2,59 +2,7 @@ import { Box, Typography } from "@mui/material";
 import { useState } from "react";
 
 const TextSample = () => {
-  const text = "Lorem ipsum dolor sit amet consectetur"
-  const [markup, setMarkup] = useState(text)
-  
-  const output: any = []
-  
-  // Function checks the location of the downclick to find 
-  // which child element we begin in.
-  const downClickHandler = (el: any) =>{
-    const i = output.from(el.parentNode.children).indexOf(el)
-    console.log(i)
-  }
-
-  // Function handles the highlighted text, highlighting it 
-  // and updating the necessary variables. 
-  const highlighterHandler = () =>{
-    const selObj = window.getSelection()
-    
-    if(selObj != null){
-      let start: number = selObj.anchorOffset
-      let end: number = selObj.focusOffset
-
-      // Allow the user to highlight text backwards
-      if(start > end){
-        let temp = end
-        end = start
-        start = temp
-      }
-
-      const before= text.substring(0, start)
-      const after = text.substring(end, text.length)
-      const select = selObj.toString()
-
-      output.push(<span>{before}</span>)
-      output.push(<mark>{select}</mark>)
-      output.push(<span>{after}</span>)
-      
-      setMarkup(output)
-    
-    }
-  }
-
-  return (
-    <Box
-    style={{
-      border:'solid',
-      borderWidth: '1px',
-      margin: '4px',
-      padding: '4px'
-    }}
-    >
-       <Typography onMouseDown={downClickHandler} onMouseUp={highlighterHandler}>{markup}</Typography>
-    </Box>
-  );
-};
+ <div>TextSample</div> 
+}
 
 export default TextSample;

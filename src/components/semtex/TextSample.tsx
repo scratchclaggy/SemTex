610e62f138ead9d5ea2f8495a38774ebba6e3a0f
Michael <m.m.downing@bigpond.com>
2022-09-24 08:11:01 +0800

    tidied up some formatting

--- src/components/semtex/TextSample.tsx
+++ src/components/semtex/TextSample.tsx
@@ -3,13 +3,37 @@ import { useAtomValue } from "jotai";
 import { useRouter } from "next/router";
 import useDataset from "src/hooks/dataset";
 import { textSampleIndexAtom } from "./Semtex";
+import { useState } from "react";
 
 const TextSample = () => {
   const router = useRouter();
   const { dataset } = useDataset(router.query.datasetID as string | undefined);
   const textSampleIndex = useAtomValue(textSampleIndexAtom);
-
+  
   const textSample = dataset?.textSamples[textSampleIndex]?.body ?? "";
+
+  const base = {start: 0, end: textSample.length, value: textSample}
+  
+  const [markup, setMarkup] = useState([textSample])
+
+  type highlightData = {
+    start: number,
+    end: number,
+    value: string
+  }
+
+  const handleMouseUp = () => {
+    const selObj = window.getSelection()
+
+    if(selObj != null && selObj.focusNode == selObj.anchorNode){
+
+      let start = selObj.anchorOffset
+      let end = selObj.focusOffset
+
+      console.log()
+    }
+    console.log("test")
+  }
 
   return (
     <Box
@@ -25,8 +49,8 @@ const TextSample = () => {
         },
       }}
     >
-      <Typography fontFamily="Roboto Mono" fontSize="24px">
-        {textSample}
+      <Typography onMouseUp={handleMouseUp}fontFamily="Roboto Mono" fontSize="24px">
+        {markup}
       </Typography>
     </Box>
   );

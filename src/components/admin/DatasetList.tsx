fc0c0a7ebf4cbda480bf51ab9a97e0518e844c85
James van de Ven <hello@jamesvandeven.com>
2022-09-25 16:10:37 +0800

    Use MUI DataGrid instead of Table

--- src/components/admin/DatasetList.tsx
+++ src/components/admin/DatasetList.tsx
@@ -1,4 +1,70 @@
+import { Alert, AlertTitle, Box, Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { atom, useAtomValue, useSetAtom } from "jotai";
+import { useMemo } from "react";
+import useDatasetList from "src/hooks/datasetList";
+
+export const filterAtom = atom<string>("");
+export const selectedDatasetIDsAtom = atom<string[]>([]);
+
 const DatasetList = () => {
-  return <div>DatasetList</div>;
+  const { datasetList, datasetListError } = useDatasetList();
+  const filter = useAtomValue(filterAtom);
+  const setSelected = useSetAtom(selectedDatasetIDsAtom);
+
+  const rows: GridRowsProp | undefined = useMemo(
+    () =>
+      datasetList
+        ?.filter((dataset) => dataset.name.toLowerCase().includes(filter))
+        .map((dataset) => {
+          return {
+            ...dataset,
+            created: new Intl.DateTimeFormat(["ban", "id"]).format(
+              dataset.created
+            ),
+          };
+        }),
+    [datasetList]
+  );
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", flex: 7 },
+    {
+      field: "created",
+      headerName: "Created",
+      flex: 2,
+      headerAlign: "center",
+      align: "center",
+    },
+  ];
+
+  return (
+    <>
+      {datasetListError && (
+        <Alert severity="error">
+          <AlertTitle>Error {datasetListError.code}</AlertTitle>
+          <Typography>{datasetListError.message}</Typography>
+          {datasetListError.details && (
+            <Typography>Details: {datasetListError.details}</Typography>
+          )}
+          {datasetListError.hint && (
+            <Typography>hint: {datasetListError.hint}</Typography>
+          )}
+        </Alert>
+      )}
+      {rows && (
+        <Box height="80vh" width="90vw" bgcolor="#F5F5F0">
+          <DataGrid
+            columns={columns}
+            rows={rows}
+            checkboxSelection
+            onSelectionModelChange={(newSelection) =>
+              setSelected(newSelection as string[])
+            }
+          />
+        </Box>
+      )}
+    </>
+  );
 };
 export default DatasetList;

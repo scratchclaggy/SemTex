6ec6f95f2a5b73f708af438a632ec855db9e8e50
Aswintvinod <22870036@student.uwa.edu.au>
2022-09-26 20:44:10 +0800

    Working delete button

--- src/components/admin/ButtonConsole.tsx
+++ src/components/admin/ButtonConsole.tsx
@@ -3,31 +3,30 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import DownloadIcon from "@mui/icons-material/Download";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import {selectedDatasetIDsAtom} from "./DatasetList"
-import {useAtomValue} from "jotai";
-import {deleteDatasets} from  "src/utils/dataset"
-
+import { useAtomValue } from "jotai";
+import { deleteDatasets } from "src/utils/dataset";
+import { selectedDatasetIDsAtom } from "./DatasetList";
 
 const ButtonConsole = () => {
- 
   const selectedDatasetIDs = useAtomValue(selectedDatasetIDsAtom);
 
-  const onDelete=() => { console.log(selectedDatasetIDs);deleteDatasets(selectedDatasetIDs)}
+  const onDelete = () => {
+    console.log(selectedDatasetIDs);
+    deleteDatasets(selectedDatasetIDs);
+  };
   return (
     <ButtonGroup size="large" aria-label="large button group" fullWidth>
-      
-    <Button >
-      <AddIcon />
-      Add Data Set
-    </Button>
-    <Button  onClick={onDelete} >
-      <DeleteIcon />
-      Delete Data Set
-    </Button>
-    <Button >
-      <DownloadIcon /> Download Data Set
-    </Button>
-
+      <Button>
+        <AddIcon />
+        Add Data Set
+      </Button>
+      <Button onClick={onDelete}>
+        <DeleteIcon />
+        Delete Data Set
+      </Button>
+      <Button>
+        <DownloadIcon /> Download Data Set
+      </Button>
     </ButtonGroup>
   );
 };

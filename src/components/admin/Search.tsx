e218f47713726613f85ebb620cb0136bab84303f
Aswintvinod <22870036@student.uwa.edu.au>
2022-09-26 17:26:38 +0800

    Working Text field

--- src/components/admin/Search.tsx
+++ src/components/admin/Search.tsx
@@ -1,4 +1,15 @@
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+import { atom, useAtom } from "jotai";
+
+
+
+
 const Search = () => {
-  return <div>Search</div>;
+  
+
+ return  <TextField id="outlined-basic" label=<SearchIcon/>   color="primary"/>
+
+ 
 };
 export default Search;

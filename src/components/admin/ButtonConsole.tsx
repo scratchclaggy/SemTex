ca7f888bb2f701991573dba6f481f96044fc8df0
Aswintvinod <22870036@student.uwa.edu.au>
2022-09-26 18:31:24 +0800

    Working Button Console

--- src/components/admin/ButtonConsole.tsx
+++ src/components/admin/ButtonConsole.tsx
@@ -1,4 +1,23 @@
+import DownloadIcon from '@mui/icons-material/Download';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+
+const buttons = [
+  <Button key="one"><AddIcon/>Add Data Set</Button>,
+  <Button key="two"><DeleteIcon/>Delete Data Set</Button>,
+  <Button key="three"><DownloadIcon/>Download Data Set </Button>,
+];
+
 const ButtonConsole = () => {
-  return <div>Button Console</div>;
+
+  return(
+    <ButtonGroup size="large" aria-label="large button group">
+  {buttons}
+</ButtonGroup>
+  )
 };
 export default ButtonConsole;
--- src/components/admin/Search.tsx
+++ src/components/admin/Search.tsx
@@ -1,7 +1,20 @@
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 import { atom, useAtom } from "jotai";
+import DatasetList from "./DatasetList";
 
+
+ // const requestSearch = (searchedVal: string) => {
+ //   const filteredRows = originalRows.filter((row) => {
+ //     return row.name.toLowerCase().includes(searchedVal.toLowerCase());
+ //   });
+ //   setRows(filteredRows);
+ // };
+//
+ // const cancelSearch = () => {
+//    setSearchTerm("");
+//    requestSearch(searchTerm);
+//  };
 
 
 

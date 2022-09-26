9215b142f183ca05cbf0a5cc0d54d3d988201e21
Aswintvinod <22870036@student.uwa.edu.au>
2022-09-26 20:52:05 +0800

    It all works

--- src/components/admin/ButtonConsole.tsx
+++ src/components/admin/ButtonConsole.tsx
@@ -4,24 +4,22 @@ import DownloadIcon from "@mui/icons-material/Download";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useAtomValue } from "jotai";
+import { useRouter } from "next/router";
 import { deleteDatasets } from "src/utils/dataset";
 import { selectedDatasetIDsAtom } from "./DatasetList";
-import { useRouter } from 'next/router'
 
 const ButtonConsole = () => {
-const selectedDatasetIDs = useAtomValue(selectedDatasetIDsAtom); 
-const router = useRouter();
+  const selectedDatasetIDs = useAtomValue(selectedDatasetIDsAtom);
+  const router = useRouter();
 
   const onDelete = () => {
     console.log(selectedDatasetIDs);
     deleteDatasets(selectedDatasetIDs);
   };
 
-
-  const onAdd=() => {  
+  const onAdd = () => {
     router.push(`admin/new-dataset`);
-
-  }
+  };
   return (
     <ButtonGroup size="large" aria-label="large button group" fullWidth>
       <Button onClick={onAdd}>

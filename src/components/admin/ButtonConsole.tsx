2ce45bdc5958061cbf3451ac26f6e649c4a820b6
Aswintvinod <22870036@student.uwa.edu.au>
2022-10-01 11:40:05 +0800

    WIP download button

--- src/components/admin/ButtonConsole.tsx
+++ src/components/admin/ButtonConsole.tsx
@@ -4,10 +4,14 @@ import DownloadIcon from "@mui/icons-material/Download";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import { useAtom } from "jotai";
+import { filter } from "lodash";
 import { useRouter } from "next/router";
 import { deleteDatasets } from "src/utils/dataset";
+import supabase from "src/utils/supabase";
+import { json } from "stream/consumers";
 import { useSWRConfig } from "swr";
 import { selectedDatasetIDsAtom } from "./DatasetList";
+
 
 const ButtonConsole = () => {
   const [selectedDatasetIDs, setSelectedDatasetIDs] = useAtom(
@@ -22,6 +26,23 @@ const ButtonConsole = () => {
     setSelectedDatasetIDs([]);
   };
 
+  
+
+  const onDownload = () => {
+    selectedDatasetIDs.forEach(async (datasetID) => {
+      
+      const respose = await supabase.rpc("download_dataset", {
+        downloaded_dataset_id: datasetID,
+  
+    
+      });
+      //const blob = new Blob([respose.data], { type: json})
+
+      console.log(respose);
+    });
+    };
+
+
   const onAdd = () => {
     router.push(`admin/new-dataset`);
   };
@@ -35,7 +56,7 @@ const ButtonConsole = () => {
         <DeleteIcon />
         Delete Data Set
       </Button>
-      <Button>
+      <Button onClick={onDownload}>
         <DownloadIcon /> Download Data Set
       </Button>
     </ButtonGroup>

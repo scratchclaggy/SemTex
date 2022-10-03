74ee3146916c7b16ad9e661a67e9b86203883bd7
Michael <m.m.downing@bigpond.com>
2022-10-03 23:52:24 +0800

    added conditional palette display

--- src/components/admin/CreateDataset/CreateHighlighterComponents/ColorPicker.tsx
+++ src/components/admin/CreateDataset/CreateHighlighterComponents/ColorPicker.tsx
@@ -1,18 +1,31 @@
+import { Box, Button, Stack } from '@mui/material';
 import { useState } from 'react';
 import { HuePicker } from 'react-color';
 
 const ColorPicker = () => {
   const [color, setColor] = useState("#ff0000")
+  const [display, setDisplay] = useState(false)
 
   const handleColorChange = (color:string) => {
     setColor(color)
   }
 
+  const handleDisplay = () => {
+    setDisplay(!display)
+  }
+
   return (
+    <Stack direction="row">
+      <Button  onClick={handleDisplay}>
+        Pick Color:
+      </Button>
+      {display && (
         <HuePicker
         color={color}
         onChange={(color) => handleColorChange(color.hex)}
         />
+      )}
+    </Stack>
     );
 };
 export default ColorPicker;

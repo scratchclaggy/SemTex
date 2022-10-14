86cc1a621765117561a2fee26fb6de14934f1351
Aswintvinod <22870036@student.uwa.edu.au>
2022-10-14 14:17:39 +0800

    FInal V

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,35 +1,26 @@
 //404 page not found
-import React from "react"
-import Link from 'next/link'
-import { Grid } from "@mui/material";
-import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
- const FourOhFour=()=> {
-    return (
-    <Grid
-  container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  justifyContent="center"
-  style={{ minHeight: '75vh' }}
->
+import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
+import { Stack } from "@mui/material";
+import Link from "next/link";
+import React from "react";
 
-  
-<Grid item m={1}>
-  <SentimentVeryDissatisfiedIcon fontSize="large" color="disabled" />
-    </Grid>
-    <Grid item m={1} >
-      <h1>404 | Page Not Found  </h1>
+const FourOhFour = () => {
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={0.5}
+      style={{ minHeight: "80vh" }}
+    >
+      <SentimentVeryDissatisfiedIcon fontSize="large" color="disabled" />
+
+      <h1>404 | Page Not Found </h1>
+
       <Link href="/">
-
-        <a>Go back home</a> 
-
-         
-        
+        <a>Go back home</a>
       </Link>
-
-    </Grid>   
-   
-   </Grid> 
-  )}
-  export default FourOhFour;
+    </Stack>
+  );
+};
+export default FourOhFour;

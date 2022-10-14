40814c4406581a5fdf927c0df33f6b8d6373afe7
Aswintvinod <22870036@student.uwa.edu.au>
2022-10-14 14:30:06 +0800

    Final PR

---
+++ src/components/admin/CreateDataset/CreateDataset.tsx
@@ -0,0 +1,19 @@
+import CreateHighlighter from "./CreateHighlighter";
+import CreateResponses from "./CreateResponses";
+
+const CreateDataset = () => {
+  const handleUpload = () => {
+    console.log("Upload");
+  };
+
+  return (
+    <form style={{ position: "absolute" }}>
+      <input type="text" placeholder="Dataset Name" />
+      <input type="text" placeholder="Dataset Instructions" />
+      <input type="button" value="Upload" onClick={handleUpload} />
+      <CreateResponses />
+      <CreateHighlighter />
+    </form>
+  );
+};
+export default CreateDataset;

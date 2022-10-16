bddb5ba9392ff9605a7850229c086dfec4fd358c
andyyyh <105978737+andyyyh@users.noreply.github.com>
2022-10-16 22:08:57 +0800

    Update user_responses.tsx

--- src/hooks/user_responses.tsx
+++ src/hooks/user_responses.tsx
@@ -1,7 +1,7 @@
 import { PostgrestError } from "@supabase/supabase-js";
+import { UserResponse } from "src/types/client";
+import supabase from "src/utils/supabase";
 import useSWR from "swr";
-import { UserResponse } from "../types/client";
-import supabase from "../utils/supabase";
 
 const useUserResponses = (datasetID: string | undefined) => {
   const { data, error, mutate } = useSWR(

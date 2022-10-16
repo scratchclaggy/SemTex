2f271869070df7bf7db084c93da8aad4d20ecb68
andyyyh <105978737+andyyyh@users.noreply.github.com>
2022-10-16 22:04:52 +0800

    Update signIn.test.tsx

--- __tests__/signIn.test.tsx
+++ __tests__/signIn.test.tsx
@@ -1,5 +1,4 @@
 import  ProgressBar from "../src/components/semtex/Progress"
-//import { useRouter } from "next/router";
 import MyApp from "../src/pages/_app";
 import {render, screen} from '@testing-library/react'
 import React, { useContext, useEffect, useState } from "react";
@@ -8,24 +7,14 @@ import '@testing-library/jest-dom'
 import { useRouter } from "next/router";
 import Link from "../src/components/ui/Link"
 import SignIn from "../src/components/auth/SignIn"
-//const useRouter = jest.spyOn(require('next/router'), 'useRouter')
-//useRouter.mockImplementation(() => {
-//  return {
-//    query:{
-//
-//    }
-//  }
-//});
 
 
 test('contain button console', async () => {
-  // ARRANGE
   render(
     <SignIn/>
   )
 
   const linkElement = screen.getByRole('Button');
   expect(screen.getByRole('Button').textContent).toBe('Previous');
-  //expect(screen.getByRole('link')).toHaveAttribute('value', '/www.google.com')
 
 })

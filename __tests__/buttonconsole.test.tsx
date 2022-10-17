2bcdba02f86b917572f179e31c3bc58119ed726b
andyyyh <105978737+andyyyh@users.noreply.github.com>
2022-10-17 10:56:04 +0800

    Delete buttonconsole.test.tsx

--- __tests__/buttonconsole.test.tsx
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import ButtonConsole from "../src/components/admin/ButtonConsole";
-
-test("contain button console", async () => {
-  render(<ButtonConsole />);
-  const linkElement = screen.getByText("Button Console");
-  expect(linkElement).toBeInTheDocument();
-});
